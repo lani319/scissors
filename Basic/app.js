38a8502c96f53dba0b2aacc93ae996d3fecdd7d2
yoonhakeo <lani319@gmail.com>
2022-04-08 10:50:22 +0900

    가위바위보 1차 완성

--- Basic/app.js
+++ Basic/app.js
@@ -7,6 +7,12 @@ data : {
     comChoice : null,
     count : 3,
     winner : null,
+    lifeOfMe : 3,
+    lifeOfCom : 3,
+    isSelectable : true,
+    isThinkable : false,
+    logs : [],
+
     
 },
 watch : {
@@ -23,31 +29,72 @@ watch : {
             }
 
             //승패 결정
-            if(this.myChoice == this.comChoice) {
+            if(this.myChoice === this.comChoice) {
                 this.winner = "draw";
 
-            } else if (this.myChoice == "scissor" && this.comChoice == "paper"){
+            } else if (this.myChoice === "scissor" && this.comChoice === "paper"){
                 this.winner = "me";
-            } else if (this.myChoice == "scissor" && this.comChoice == "rock"){
+            } else if (this.myChoice === "scissor" && this.comChoice === "rock"){
                 this.winner = "com";
-            } else if (this.myChoice == "rock" && this.comChoice == "scissor"){
+            } else if (this.myChoice === "rock" && this.comChoice === "scissor"){
                 this.winner = "me";
-            } else if (this.myChoice == "rock" && this.comChoice == "paper"){
+            } else if (this.myChoice === "rock" && this.comChoice === "paper"){
                 this.winner = "com";
-            } else if (this.myChoice == "paper" && this.comChoice == "rock"){
+            } else if (this.myChoice === "paper" && this.comChoice === "rock"){
                 this.winner = "me";
-            } else if (this.myChoice == "paper" && this.comChoice == "scissor"){
+            } else if (this.myChoice === "paper" && this.comChoice === "scissor"){
                 this.winner = "com";
             } else { 
                 this.winner = "error";
             }
             console.log(this.winner);
+
+            if(this.winner === "me"){
+                this.lifeOfCom--;
+            }else if(this.winner ==="com"){
+                this.lifeOfMe--;
+            }
+            this.count = 3;
+            this.isSelectable = true;
+            this.isThinkable = false;
+
+            //결과값 기록
+            let log = {
+
+                message : "You "+this.myChoice + 
+                          " Com "+this.comChoice + 
+                          " Winner is " + this.winner,
+                winner : this.winner,
+            }
+            //this.logs.push(log);
+            this.logs.unshift(log);
+
+        
+        }
+    },
+    lifeOfMe : function(newVal){
+        if(newVal === 0){
+            setTimeout(() => {
+                confirm("패배했습니다.");
+                location.href = '';    
+            }, 500);
+            
+        }
+    },
+    lifeOfCom : function(newVal){
+        if(newVal === 0){
+            setTimeout(() => {
+                confirm("승리했습니다.");
+                location.href = '';    
+            }, 500);
+            
         }
     }
 },
 methods : {
     startGame : function() {
-    
+        this.isSelectable = false;
+        this.isThinkable = true;
         if(this.myChoice === null){
             alert("값을 선택해주세요");
         }
@@ -62,7 +109,7 @@ methods : {
                 }
             },1000);
 
-            this.count = 3;
+            
      }
         
     }
