e9fc18687c1d3e300cd4094085790a36a5ed6fad
yoonhakeo <lani319@gmail.com>
2022-04-08 09:59:33 +0900

    3rd

--- Basic/app.js
+++ Basic/app.js
@@ -4,7 +4,46 @@ el : "#app",
 data : {
     test :  "test1234",
     myChoice : null ,
-    gameCount : 5,
+    comChoice : null,
+    count : 3,
+    winner : null,
+    
+},
+watch : {
+    count : function(newVal){
+        if (newVal === 0){
+            let rNum = Math.floor( ( Math.random() * 10 ) );
+            
+            if(rNum > 0 && rNum < 4){
+                this.comChoice = "scissor";
+            }else if (rNum > 4 && rNum < 7){
+                this.comChoice = "rock";
+            }else {
+                this.comChoice = "paper";
+            }
+
+            //승패 결정
+            if(this.myChoice == this.comChoice) {
+                this.winner = "draw";
+
+            } else if (this.myChoice == "scissor" && this.comChoice == "paper"){
+                this.winner = "me";
+            } else if (this.myChoice == "scissor" && this.comChoice == "rock"){
+                this.winner = "com";
+            } else if (this.myChoice == "rock" && this.comChoice == "scissor"){
+                this.winner = "me";
+            } else if (this.myChoice == "rock" && this.comChoice == "paper"){
+                this.winner = "com";
+            } else if (this.myChoice == "paper" && this.comChoice == "rock"){
+                this.winner = "me";
+            } else if (this.myChoice == "paper" && this.comChoice == "scissor"){
+                this.winner = "com";
+            } else { 
+                this.winner = "error";
+            }
+            console.log(this.winner);
+        }
+    }
 },
 methods : {
     startGame : function() {
@@ -16,12 +55,14 @@ methods : {
             //alert(this.myChoice);
             let thisCount = setInterval(()=>{
                 
-                this.gameCount -- ;
+                this.count -- ;
 
-                if(this.gameCount === 0){
+                if(this.count === 0){
                     clearInterval(thisCount);
                 }
             },1000);
+
+            this.count = 3;
      }
         
     }
